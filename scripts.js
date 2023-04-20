7a0b45111adb71c56f8ecdab711979c3b3f32c98
Haseab <haseab.amin@gmail.com>
2023-04-20 11:17:30 -0400

    remove debug printing

--- scripts.js
+++ scripts.js
@@ -287,7 +287,7 @@ const getTweets = async () => {
 
 const trackedTweets = {};
 let isEnabled = false;
-const debug = true;
+// const debug = true;
 let sensitivity = 30;
 
 // chrome.storage.sync.clear(() => {
